4638f34554474045b868f658082821bc1a79a285
lukasz_sobola <lukasz.sobola@prime-force.com>
2019-12-20 07:35:28 +0100

    fetching data from swapi api

--- SimpleGraphTest/src/app/app.component.ts
+++ SimpleGraphTest/src/app/app.component.ts
@@ -1,20 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import * as swapiModule from 'swapi-wrapper';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SimpleGraphTest';
   apiUrl = 'https://swapi.co/api/people/';
+  private people;
 
   ngOnInit(): void {
-    fetch(this.apiUrl).then(resp => {
-      console.log(resp);
-  })
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
+    new Promise((resolve, reject) => {
+      this.getPeople(this.apiUrl, [], resolve, reject);
+    })
+    .then(response => {
+      this.people = response;
+      console.log(this.people);
+    });
+  }
 
+  private getPeople = (url, people, resolve, reject) => {
+    fetch(url)
+    .then(resp => resp.json())
+    .then(data => {
+      const retrivedPeople = people.concat(data.results);
+      if (data.next !== null) {
+        this.getPeople(data.next, retrivedPeople, resolve, reject);
+      } else {
+        resolve(retrivedPeople);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      reject(err);
+    });
   }
 }
